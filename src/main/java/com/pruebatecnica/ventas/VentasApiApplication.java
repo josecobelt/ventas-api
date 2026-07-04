ebd6e695f4a09cf356bdbf19109a1b971aa8a68a
josecobelt <josejuanb.fim@ms.uas.edu.mx>
2026-07-04 04:39:58 +0000

    feat: clase principal de la aplicacion y configuracion base (application.yml)

---
+++ src/main/java/com/pruebatecnica/ventas/VentasApiApplication.java
@@ -0,0 +1,12 @@
+package com.pruebatecnica.ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VentasApiApplication.class, args);
+    }
+}
